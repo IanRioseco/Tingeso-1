d896433b7d37883ec950a81beffd62b6c7cf2248
ian rioseco <ian.rioseco@usach.cl>
2026-05-06 23:13:58 -0400

    nuevos cambios para rehacer el despliegue

--- travelagencyfrontend/src/main.jsx
+++ travelagencyfrontend/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ReactKeycloakProvider } from '@react-keycloak/web';
-import './polyfills/webCrypto.js';
 import './index.css';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
