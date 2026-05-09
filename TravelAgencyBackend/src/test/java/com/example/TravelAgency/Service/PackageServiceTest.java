12effa99b45a00416f4c932741712e914891cc40
ian rioseco <ian.rioseco@usach.cl>
2026-05-09 19:59:53 -0400

    cambios para las urls de direccion de keycloack

--- TravelAgencyBackend/src/test/java/com/example/TravelAgency/Service/PackageServiceTest.java
+++ TravelAgencyBackend/src/test/java/com/example/TravelAgency/Service/PackageServiceTest.java
@@ -18,6 +18,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
@@ -139,4 +140,3 @@ class PackageServiceTest {
         assertThat(result).extracting(PackageEntity::getId).containsExactly(1L);
     }
 }
-
